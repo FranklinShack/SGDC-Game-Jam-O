f56fd1a6bcd3f7b9b48634b4025d5b039776f422
RandalfTheRed <stephentschmidt@gmail.com>
2021-10-23 13:01:08 -0400

    Fixed Error

--- Node2D.gd
+++ Node2D.gd
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	var space_rid = get_world_2d().space
 	var space_state = Physics2DServer.space_get_direct_state(space_rid)
 	var result = space_state.intersect_ray(global_position, Player.global_position, [self])
-	if result.get_collider() == Player:
+	if result.collider == Player:
 		print("I SEE DABABY!!!")
 	else:
 		print("wHeRe Da BaBy?1!/?1!")
