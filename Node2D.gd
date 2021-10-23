29caacb01cfbeda7ed704b425997a9db364e3639
Limey24 <bllmd1@gmail.com>
2021-10-23 13:02:59 -0400

    Vision attempt two

--- Node2D.gd
+++ Node2D.gd
@@ -17,7 +17,12 @@ func _physics_process(delta):
 	var space_rid = get_world_2d().space
 	var space_state = Physics2DServer.space_get_direct_state(space_rid)
 	var result = space_state.intersect_ray(global_position, Player.global_position, [self])
-	if result.collider == Player:
+	var seen = true
+	for item in result:
+		if result.collider != Player:
+			seen = false
+			break
+	if seen:
 		print("I SEE DABABY!!!")
 	else:
 		print("wHeRe Da BaBy?1!/?1!")
